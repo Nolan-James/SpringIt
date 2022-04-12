dcf735ccdb9b7511bd59c2c8278f444a1b223668
Nolan-James <nj@nolanjames.me>
2022-04-12 16:05:06 +0100

    Initial Commit

---
+++ src/test/java/me/nolanjames/springit/SpringItApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nolanjames.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
